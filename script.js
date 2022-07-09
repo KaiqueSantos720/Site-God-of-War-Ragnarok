e6e9fd30544463e8ae7d0fdbbb74c7cba9536f5d
Kaique Santos <kaique.santos20042018@gmail.com>
2022-07-09 09:40:46 -0300

    alteracoes nos efeitos do site

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-$("#navbar a").on("click", function(event){
-    if(this.hash !== ""){
-        event.preventDefault();
-
-        const hash = this.hash;
-
-        $("html, body").animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
-});
-
-
 window.addEventListener("scroll", function(){
     if(this.window.scrollY > 150){
         this.document.querySelector("#navbar").style.opacity = 0.9
